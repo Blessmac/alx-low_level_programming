13ff775208f518651ca9a84546e6d57b11380dd3
Blessmac <omodeleblessing23@gmail.com>
2022-11-16 12:05:35 -0800

    task3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,8 +1,10 @@
-#ifndef _CALC_H_
-#define _CALC_H_
+#ifndef CALC_H
+#define CALC_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct op - Struct op
  *
@@ -13,7 +15,7 @@ typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-} op_t;
+} op_t
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
